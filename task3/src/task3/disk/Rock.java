862f255c15e42e17d9afe9190af127e4a8b4eadc
Viktoriya Shulkevich <37610932+Vika-Chernika@users.noreply.github.com>
2018-10-10 05:01:46 +0300

    Add files via upload

---
+++ task3/src/task3/disk/Rock.java
@@ -0,0 +1,16 @@
+package task3.disk;
+
+import task3.disk.Composition;
+
+
+public class Rock extends Composition {
+	
+	public Rock () {
+		super(5, "rock");
+	}
+	
+	public Rock (int duration) {
+		super(duration, "Rock");
+	}
+	
+}
